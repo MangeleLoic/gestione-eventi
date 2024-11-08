86359b342936b594010cdb02bffed4e49907ad8e
sudonona <80674485+sudonona@users.noreply.github.com>
2024-11-08 14:55:44 +0100

    first commit

---
+++ src/test/java/loicMangele/gestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package loicMangele.gestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
